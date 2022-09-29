1f473cf243794ca40b234de153f6f7e8352430a3
Wes Guirra <wesleyguirra@users.noreply.github.com>
2022-09-28 23:16:12 -0300

    Update banco_de_dados.sql

--- code/banco_de_dados.sql
+++ code/banco_de_dados.sql
@@ -1,12 +1,14 @@
 -- Criar a base de dados "Dados_RFB"
-CREATE DATABASE "Dados_RFB"
+CREATE DATABASE rfb
     WITH
     OWNER = postgres
     ENCODING = 'UTF8'
     CONNECTION LIMIT = -1;
 
-COMMENT ON DATABASE "Dados_RFB"
+COMMENT ON DATABASE rfb
     IS 'Base de dados para gravar os dados públicos de CNPJ da Receita Federal do Brasil';
+    
+GRANT ALL ON DATABASE rfb TO receita;
 
 -- Diretório físico do banco de dados:
 --SHOW data_directory;
